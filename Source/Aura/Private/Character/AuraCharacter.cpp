baf8a7196a1141209e14d00ebfa127e1a2dcb61c
ChrisBuckley1205 <chris.d.buckley@gmail.com>
2025-01-08 13:06:31 -0500

    Game Mode, also added Spring arm and camera component and fixed animation for Aura

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,17 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	GetCharacterMovement()->bConstrainToPlane = true; // good for top down games
+	GetCharacterMovement()->bSnapToPlaneAtStart = true; // good for top down game
+
+	// Character should not use Controllers Rotation:
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}

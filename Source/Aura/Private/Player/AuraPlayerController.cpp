8469be94d57c1b893c80109f2c52a710f41e2558
ChrisBuckley1205 <chris.d.buckley@gmail.com>
2025-01-08 19:12:42 -0500

    Added logic for highlighting enemies in the game world.

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,10 +4,78 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true; // For when an entity changes, good for data updating from server to clients.
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+	
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility,false,CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+
+	/**
+	 * Line trace from cursor, several scenarios:
+	 * A. Lastactor is null && ThisActor is null
+	 *	-Do Nothing
+	 *	B. LastActor is null && ThisActor is valid
+	 *	 - Highlight ThisActor
+	 *	C. LastActor is valid && ThisActor is null
+	 *	  - UnHighlight LastActor
+	 *	D. Both Actor valid, but LastActor != ThisActor
+	 *	  - UnHighlight LastActor and HighLight this actor
+	 *	E. LastActor && ThisActor is valid and same enemy
+	 *	  -	Do Nothing
+	 */	
+	if (LastActor == nullptr) 
+	{
+		if (ThisActor != nullptr)
+		{
+			//  Case B
+			ThisActor->HighlightActor();
+		}
+		else
+		{
+			// Case A, both are null, do Nothing
+		}
+	}
+	else // LastActor is valid
+	{
+		if (ThisActor == nullptr)
+		{
+			// Case C
+			LastActor->UnHighlightActor();
+		}
+		else // Both Actors are Valid
+		{
+			if (LastActor != ThisActor)
+			{
+				// Case D
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+			else
+			{
+				// case E - Do Nothing.
+			}
+		}
+	}
+	
+	
 }
 
 void AAuraPlayerController::BeginPlay()
